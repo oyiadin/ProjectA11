a2bdacd67243c9b3f824cdbd219c6a4c6b71e431
oyiadin <oyiadin@gmail.com>
2019-01-22 00:28:54 +0800

    [add] #2: manage.py

---
+++ manage.py
@@ -0,0 +1,43 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='project_a11')
+
+parser.add_argument('--server', default=False, action='store_const', const=True)
+parser.add_argument('--db_init', default=False, action='store_const', const=True)
+parser.add_argument('--config', default='config.json')
+
+
+class MyDict(dict):
+    def __getattr__(self, item):
+        value = self[item]
+        if isinstance(value, dict):
+            return MyDict(value)
+        return value
+
+
+def load_config(filename):
+    with open(filename) as f:
+        default_config = dict(
+            port=8888,
+        )
+        loaded_config = json.load(f)
+        default_config.update(loaded_config)
+        return MyDict(default_config)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    conf = load_config(args.config)
+
+    try:
+        if args.server:
+            import projecta11
+            projecta11.startup(conf)
+
+        if args.db_init:
+            import projecta11.db
+            projecta11.db.init_db(conf)
+
+    except KeyboardInterrupt:
+        print('goodbye')

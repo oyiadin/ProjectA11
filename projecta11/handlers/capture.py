0f93b5acd65b34e421877e388a85e15d5049d8c1
BrownFly <1291049794@qq.com>
2019-01-25 20:54:23 +0800

    前端验证码(未完成)

---
+++ projecta11/handlers/capture.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from projecta11.handlers.base import BaseHandler
+from projecta11.routers import handling
+from projecta11.handlers import check_code
+
+import io
+
+
+@handling(r"/check_code.*")
+class CheckCodeHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        image, CODE = check_code.create_validate_code()
+        # BytesIO操作二进制数据，将验证码图形写入内存
+        mstream = io.BytesIO()
+        image.save(mstream, 'GIF')
+        self.write(mstream.getvalue())

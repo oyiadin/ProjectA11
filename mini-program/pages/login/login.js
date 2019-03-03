d4626bd15682d43ebffa6517af91f4ae24a95652
oyiadin <oyiadin@gmail.com>
2019-03-03 21:31:56 +0800

    #14 登陆界面精修

--- mini-program/pages/login/login.js
+++ mini-program/pages/login/login.js
@@ -1,91 +1,13 @@
-// pages/login/login.js
-var app = getApp();
-
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    username: null,
-    password: null,
+    account_types: ["学生", "教师"],
+    account_types_name: ["学号", "教职工号"],
+    account_type_index: 0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    if(app.appData.userInfo == null ){
-      wx.redirectTo("../register/register");
-    }
-    else {
-      this.setDate({username:app.appData.username})
-    }
+  bind_account_type_change: function (e) {
+    this.setData({
+      account_type_index: e.detail.value
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  loginBtnClick:function() {
-    app.appData.userInfo = {username:this.data.username,password:this.data.password}
-  },
-
-  registerBtnClick:function() {
-    
-  },
-
-  usernameInput:function(event) {
-    this.setDate({username:event.detail.value})
-  },
-
-  passwordInput: function (event) {
-    this.setDate({ password: event.detail.value })
-  }
-  
-})
+});

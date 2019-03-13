123cb9570cdf59a44f0d8775d020f4b8d4e9ec30
oyiadin <oyiadin@gmail.com>
2019-03-13 13:39:42 +0800

    用户中心前后端交互

--- mini-program/pages/user/user-information/user-information.js
+++ mini-program/pages/user/user-information/user-information.js
@@ -3,45 +3,17 @@ var u = require('../../../utils/utils.js');
 
 Page({
   data: {
-    session_id: "null",
-    name: '用户名Test',
-    gender: "男",
-    student_number: 17071140,
-    mobile: ""
-  },
-
-  get_information: function (session){
-    const that = this;
-    u.request(
-        'POST', '/user/' + this.session,
-        {
-          staff_id: this.session,
-        },
-        function (res) {
-          return res.data.information
-        },
-        function (res) {
-            pass;
-        })
+    response: {},
   },
 
   onLoad: function (options) {
-    var session = wx.getStorageSync('session_id');
-    var is_login = wx.getStorageSync('is_login');
-    if (is_login) {
-      var information = get_information(session);
-      this.setData({
-        session_id:session,
-        name: information.name,
-        gender: information.gender,
-        student_number: information.number,
-        mobile: information.phone_number
-      })
-    }else{
-      wx.navigateTo({
-        url: '../../login/login',
-      })
-    }
+    const that = this;
+    u.request(
+      'GET', '/user', {},
+      function (res) {
+        that.setData({ data: res.data });
+      }
+    );
   },
 
 })

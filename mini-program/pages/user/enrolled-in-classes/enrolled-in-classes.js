c1a7b99dfed5f81e4d472e657f529800734929e3
BrownFly <1291049794@qq.com>
2019-03-14 13:35:41 +0800

    个人课程页面查看实现

--- mini-program/pages/user/enrolled-in-classes/enrolled-in-classes.js
+++ mini-program/pages/user/enrolled-in-classes/enrolled-in-classes.js
@@ -4,7 +4,9 @@ var u = require('../../../utils/utils.js');
 Page({
   data: {
     colors: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
-    classes: [],
+    classes: [
+      { "weekday": 2, "start": 1, "end": 2, "class_name": "数学分析@教A-301"}
+    ],
   },
 
   onLoad: function() {

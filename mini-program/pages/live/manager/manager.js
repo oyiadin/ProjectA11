396b897e8625aa76678d194e112e04f9414b7107
BrownFly <1291049794@qq.com>
2019-03-18 19:13:35 +0800

    观看视频页面

--- mini-program/pages/live/manager/manager.js
+++ mini-program/pages/live/manager/manager.js
@@ -8,6 +8,10 @@ Page({
         title: '创建直播',
         url: './live_create/live_create',
       },
+      {
+        title: '已创建直播',
+        url: './live_exists/live_exists',
+      }
     ]
   },
 

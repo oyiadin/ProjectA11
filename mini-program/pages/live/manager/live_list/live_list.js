218606c0cbadb3b87ada973038cb4d4f3518d9d5
BrownFly <1291049794@qq.com>
2019-03-18 19:52:45 +0800

    修改下

--- mini-program/pages/live/manager/live_list/live_list.js
+++ mini-program/pages/live/manager/live_list/live_list.js
@@ -1,4 +1,7 @@
 // pages/live/manager/live_list/live_list.js
+var u = getApp().utils;
+
+
 Page({
 
   /**
@@ -12,7 +15,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var role = wx.getStorageSync('role');
+    this.setData({ role: role });
+    this.refresh();
   },
 
   /**

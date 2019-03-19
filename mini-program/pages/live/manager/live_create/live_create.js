ef32833c3712ce3971fdfd81483b7c8260a81dcb
BrownFly <1291049794@qq.com>
2019-03-19 14:03:17 +0800

    直播课程

--- mini-program/pages/live/manager/live_create/live_create.js
+++ mini-program/pages/live/manager/live_create/live_create.js
@@ -4,25 +4,49 @@ var u = getApp().utils;
 Page({
   data: {
     is_checked: false,
-    to_class_name: "", // 对应的课程名
+    classItems: [
+      { name: '302班.', value: '0', checked: true },
+      { name: '301班', value: '1' }
+    ],
+    open_class_name: "",
     live_name: "",
     description: "",
     start_date: "2018-2-2",
-    start_time: "10:30",
+    start_time: "12:00",
     time_active: 0,
-    time_list: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+    time_list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+  },
+  // 班级选择
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var classItems = this.data.classItems, values = e.detail.value;
+    for (var i = 0, lenI = classItems.length; i < lenI; ++i) {
+      classItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (classItems[i].value == values[j]) {
+          classItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      classItems: classItems
+    });
   },
   // 开关选项
-  changeSwitch: function(e) {
+  changeSwitch: function (e) {
     //console.log(e.detail.value);
     this.setData({
       is_checked: e.detail.value
     })
   },
   // 面向班级名称
-  to_class_name: function(e){
+  to_class_name: function (e) {
     this.setData({
-      to_class_name: e.detail.value
+      open_class_name: e.detail.value
     })
   },
 
@@ -33,7 +57,7 @@ Page({
     })
   },
   // 简介
-  set_live_description: function(e) {
+  set_live_description: function (e) {
     //console.log(e.detail.value);
     this.setData({
       description: e.detail.value
@@ -53,7 +77,7 @@ Page({
   },
 
   // 持续时间
-  bindDateTimeActiveChange: function(e){
+  bindDateTimeActiveChange: function (e) {
     this.setData({
       time_active: parseInt(e.detail.value) + 1
     })
@@ -62,7 +86,7 @@ Page({
   // 创建课程
   do_create: function () {
     // 开始时间
-    var start_time = this.data.start_date + ' ' +this.data.start_time;
+    var start_time = this.data.start_date + ' ' + this.data.start_time;
     console.log(start_time)
     // 持续时间
     var time_active = parseInt(this.data.time_active) * 60;
--- mini-program/pages/live/manager/manager.js
+++ mini-program/pages/live/manager/manager.js
@@ -2,20 +2,25 @@ var u = getApp().utils;
 
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    apps: [
-      {
-        title: '创建直播',
-        url: './live_create/live_create',
-      },
-      {
-        title: '已创建直播',
-        url: './live_list/live_list',
-      }
-    ]
+    live_class: {
+      name : '小圆课堂',
+      start : "2018-9-10 10:23",
+      end : "2019-10-10 23:23"
+    },
   },
 
-  onLoad: function() {
-    
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var role = wx.getStorageSync('role');
+    this.setData({ role: role });
+    this.refresh();
   }
-});
+
+})
